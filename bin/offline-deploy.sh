464d406bf5330ad9b08b9d888f87448808011881
Arian van Putten <arian.vanputten@gmail.com>
2021-02-25 22:22:54 +0100

    Add continous delivery for testing offline

--- bin/offline-deploy.sh
+++ bin/offline-deploy.sh
@@ -10,7 +10,7 @@ export ZAUTH_CONTAINER
 
 WSD_CONTAINER=$(sudo docker load -i $SCRIPT_DIR/../containers-adminhost/container-wire-server-deploy.tgz | awk '{print $3}')
 
-sudo docker run -it --network=host -v $HOME/.ssh:/root/.ssh -v $PWD:/wire-server-deploy $WSD_CONTAINER ./bin/secrets.sh
+./bin/secrets.sh
 
 sudo docker run -it --network=host -v $HOME/.ssh:/root/.ssh -v $PWD:/wire-server-deploy $WSD_CONTAINER ./bin/offline-cluster.sh
 sudo docker run -it --network=host -v $HOME/.ssh:/root/.ssh -v $PWD:/wire-server-deploy $WSD_CONTAINER ./bin/offline-helm.sh
